0618521d0773e701eafc900885de9b3a14482852
SashaRedka <dev.redka@gmail.com>
2021-03-16 11:33:46 +0200

    added storage functionality

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import React from 'react';
+import Storage from './utils/Storage'
+
 import CreateItem from './ToDo/CreateItem';
 import ToDoList from './ToDo/ToDoList';
 
 function App() {
-  const [todos, setTodos] = React.useState([
-    {
-      id: 1, completed: false, title: 'Test task'
-    },
-    {
-      id: 2, completed: false, title: 'Test task 2'
-    },
-    {
-      id: 3, completed: false, title: 'Test task 3'
-    }
-  ])
+  const [todos, setTodos] = React.useState(Storage.get('todos') || [])
 
   const toggleTodo = (id) => {
-    setTodos(todos.map(todo => {
+    const updatedTodos = todos.map(todo => {
       if (todo.id === id) {
         todo.completed = !todo.completed;
       }
 
       return todo;
-    }))
+    });
+    
+    setTodos(updatedTodos);
+
+    Storage.save('todos', updatedTodos);
   };
 
   const deleteTodo = (id) => {
-    setTodos(todos.filter(todo => todo.id !== id))
+    const updatedTodos = todos.filter(todo => todo.id !== id);
+
+    setTodos(updatedTodos);
+
+    Storage.save('todos', updatedTodos);
   };
 
   const addNewTodo = (title) => {
-    setTodos([...todos, { id: new Date().getTime(), completed: false, title }])
+    const updatedTodos = [...todos, { id: new Date().getTime(), completed: false, title }];
+ 
+    setTodos(updatedTodos);
+
+    Storage.save('todos', updatedTodos);
   };
 
   return (
